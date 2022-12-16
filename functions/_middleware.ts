7811060a482ed7451cab94da2f4a567eec1401d6
yhorian <liam.jm.martin@gmail.com>
2022-12-16 22:38:11 +0000

    documentation added

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,8 +2,12 @@
 
 import staticFormsPlugin from "@cardiff.marketing/pages-plugin-static-forms";
 
+
 export const onRequest: PagesFunction = staticFormsPlugin({
   respondWith: ({ formData, name }) => {
+    // Play with the form data here.
+    // Name: Refers to the name of the form set by the "data-static-form-name" attribute.
+    // Use they input fields name as a key to retrieve values from formData
     const email = formData.get('email')
     return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)
   }
