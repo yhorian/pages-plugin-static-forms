0e7bde4006cc8c56faca2dc8ff502c72cbcec1fa
yhorian <liam.jm.martin@gmail.com>
2022-12-16 21:56:25 +0000

    bugfix

--- index.js
+++ index.js
@@ -399,7 +399,7 @@ function template_plugin_default(pluginArgs) {
           waitUntil: workerContext.waitUntil.bind(workerContext)
         };
         const response = await handler(context);
-        return new Response([101, 204, 205, 304].includes(response.status) ? null : response.body, { ...response, headers: new Headers(response.headers) });
+        return response
       } else {
         return next();
       }
